6a9cb3e5fd4ff9107115fb58eb76674bec27aa25
archanam2 <maliarchanabm@gmail.com>
2020-12-11 17:12:33 +0530

    redux store

---
+++ src/reducers/contactReducer.js
@@ -0,0 +1,300 @@
+import {
+  CREATE_CONTACT,
+  GET_CONTACT,
+  UPDATE_CONTACT,
+  DELETE_CONTACT,
+  SELECT_CONTACT,
+  CLEAR_CONTACT,
+  DELETE_SELECTED_CONTACT,
+} from "../constant/types";
+
+const intialState = {
+  contacts: [
+    {
+      id: 1,
+      name: "Archana Mali",
+      username: "Bret",
+      email: "maliarchanabm@gmail.com",
+      address: {
+        street: "Kulas Light",
+        suite: "Apt. 556",
+        city: "Gwenborough",
+        zipcode: "92998-3874",
+        geo: {
+          lat: "-37.3159",
+          lng: "81.1496",
+        },
+      },
+      phone: "+91 9503026401",
+      website: "hildegard.org",
+      company: {
+        name: "Romaguera-Crona",
+        catchPhrase: "Multi-layered client-server neural-net",
+        bs: "harness real-time e-markets",
+      },
+    },
+    {
+      id: 2,
+      name: "Pooja Yadav",
+      username: "Antonette",
+      email: "py91197@gmail.com",
+      address: {
+        street: "Victor Plains",
+        suite: "Suite 879",
+        city: "Wisokyburgh",
+        zipcode: "90566-7771",
+        geo: {
+          lat: "-43.9509",
+          lng: "-34.4618",
+        },
+      },
+      phone: "+91 97621 26451",
+      website: "anastasia.net",
+      company: {
+        name: "Deckow-Crist",
+        catchPhrase: "Proactive didactic contingency",
+        bs: "synergize scalable supply-chains",
+      },
+    },
+    {
+      id: 3,
+      name: "Snehal Jadhav",
+      username: "Samantha",
+      email: "jadhavsnehal1997@gmail.com",
+      address: {
+        street: "Douglas Extension",
+        suite: "Suite 847",
+        city: "McKenziehaven",
+        zipcode: "59590-4157",
+        geo: {
+          lat: "-68.6102",
+          lng: "-47.0653",
+        },
+      },
+      phone: "+91 77439 27871",
+      website: "ramiro.info",
+      company: {
+        name: "Romaguera-Jacobson",
+        catchPhrase: "Face to face bifurcated interface",
+        bs: "e-enable strategic applications",
+      },
+    },
+    {
+      id: 4,
+      name: "Pragati Pujari",
+      username: "Karianne",
+      email: "pujaripragati3301@gmail.com",
+      address: {
+        street: "Hoeger Mall",
+        suite: "Apt. 692",
+        city: "South Elvis",
+        zipcode: "53919-4257",
+        geo: {
+          lat: "29.4572",
+          lng: "-164.2990",
+        },
+      },
+      phone: "+91 9130334968",
+      website: "kale.biz",
+      company: {
+        name: "Robel-Corkery",
+        catchPhrase: "Multi-tiered zero tolerance productivity",
+        bs: "transition cutting-edge web services",
+      },
+    },
+    {
+      id: 5,
+      name: "Swapnali Pawar",
+      username: "Kamren",
+      email: "pawarswpnali972@gmail.com",
+      address: {
+        street: "Skiles Walks",
+        suite: "Suite 351",
+        city: "Roscoeview",
+        zipcode: "33263",
+        geo: {
+          lat: "-31.8129",
+          lng: "62.5342",
+        },
+      },
+      phone: "+91 9156136847",
+      website: "demarco.info",
+      company: {
+        name: "Keebler LLC",
+        catchPhrase: "User-centric fault-tolerant solution",
+        bs: "revolutionize end-to-end systems",
+      },
+    },
+    {
+      id: 6,
+      name: "Deepali Mane",
+      username: "Leopoldo_Corkery",
+      email: "manedeepali98@gmail.com",
+      address: {
+        street: "Norberto Crossing",
+        suite: "Apt. 950",
+        city: "South Christy",
+        zipcode: "23505-1337",
+        geo: {
+          lat: "-71.4197",
+          lng: "71.7478",
+        },
+      },
+      phone: "+91 9869369951",
+      website: "ola.org",
+      company: {
+        name: "Considine-Lockman",
+        catchPhrase: "Synchronised bottom-line interface",
+        bs: "e-enable innovative applications",
+      },
+    },
+    {
+      id: 7,
+      name: "Priya Pawar",
+      username: "Elwyn.Skiles",
+      email: "pawarpriya43@gmail.com",
+      address: {
+        street: "Rex Trail",
+        suite: "Suite 280",
+        city: "Howemouth",
+        zipcode: "58804-1099",
+        geo: {
+          lat: "24.8918",
+          lng: "21.8984",
+        },
+      },
+      phone: "+91 9421125589",
+      website: "elvis.io",
+      company: {
+        name: "Johns Group",
+        catchPhrase: "Configurable multimedia task-force",
+        bs: "generate enterprise e-tailers",
+      },
+    },
+    {
+      id: 8,
+      name: "Rupali Kare",
+      username: "Maxime_Nienow",
+      email: "karerupali632@gmail.com",
+      address: {
+        street: "Ellsworth Summit",
+        suite: "Suite 729",
+        city: "Aliyaview",
+        zipcode: "45169",
+        geo: {
+          lat: "-14.3990",
+          lng: "-120.7677",
+        },
+      },
+      phone: "+91 7499921232",
+      website: "jacynthe.com",
+      company: {
+        name: "Abernathy Group",
+        catchPhrase: "Implemented secondary concept",
+        bs: "e-enable extensible e-tailers",
+      },
+    },
+    {
+      id: 9,
+      name: "Pagati Chavan",
+      username: "Delphine",
+      email: "chavanpragati@gmail.com",
+      address: {
+        street: "Dayna Park",
+        suite: "Suite 449",
+        city: "Bartholomebury",
+        zipcode: "76495-3109",
+        geo: {
+          lat: "24.6463",
+          lng: "-168.8889",
+        },
+      },
+      phone: "+91 7507892616",
+      website: "conrad.com",
+      company: {
+        name: "Yost and Sons",
+        catchPhrase: "Switchable contextually-based project",
+        bs: "aggregate real-time technologies",
+      },
+    },
+    {
+      id: 10,
+      name: "Minakshi Bhosale",
+      username: "Moriah.Stanton",
+      email: "bhosaleminakshi@gmail.com",
+      address: {
+        street: "Kattie Turnpike",
+        suite: "Suite 198",
+        city: "Lebsackbury",
+        zipcode: "31428-2261",
+        geo: {
+          lat: "-38.2386",
+          lng: "57.2232",
+        },
+      },
+      phone: "+91 9765677654",
+      website: "ambrose.net",
+      company: {
+        name: "Hoeger LLC",
+        catchPhrase: "Centralized empowering task-force",
+        bs: "target end-to-end models",
+      },
+    },
+  ],
+  contact: null,
+  selectedContacts: [],
+};
+
+export const contactReducer = (state = intialState, action) => {
+  switch (action.type) {
+    case CREATE_CONTACT:
+      return {
+        ...state,
+        contacts: [action.payload, ...state.contacts],
+      };
+    case GET_CONTACT:
+      let arr = state.contacts.filter(
+        (contact) => contact.id === action.payload
+      );
+      arr = arr.values();
+      for (let val of arr) {
+        arr = val;
+      }
+      return {
+        ...state,
+        contact: arr,
+      };
+    case UPDATE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.map((contact) =>
+          contact.id === action.payload.id ? action.payload : contact
+        ),
+      };
+    case DELETE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          (contact) => contact.id !== action.payload
+        ),
+      };
+    case DELETE_SELECTED_CONTACT:
+      return {
+        ...state,
+        contacts: [],
+      };
+    case SELECT_CONTACT:
+      return {
+        ...state,
+        selectedContacts: action.payload,
+      };
+
+    case CLEAR_CONTACT:
+      return {
+        ...state,
+        selectedContacts: [],
+      };
+    default:
+      return state;
+  }
+};
